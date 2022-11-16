c94f53a49d544cca27d51cf11bc3c7e30b24efc9
John Reid <john@noizebox.co.uk>
2022-11-16 18:40:26 +0000

    part one

---
+++ aoc_2015_day_4/src/main.rs
@@ -0,0 +1,17 @@
+use md5;
+
+fn main() {
+    let salt = String::from("ckczppom");
+    let mut count = 0;
+
+    let result = loop {
+        count += 1;
+        let digest = md5::compute(format!("{}{}", salt, count));
+        //println!("{} {:x}", count, digest);
+        if format!("{:x}", digest).starts_with("00000") {
+            break count;
+        }
+    };
+
+    println!("{}", result);
+}
